82876b7312b54750cfef486af42103addf3ea595
Volodymyr-Cherniak <vovastalker26@gmail.com>
2020-02-03 19:29:37 +0100

    Updates

---
+++ precache-manifest.dac9ad6a650ad9f8b903fe3c2a185f9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65981233f8eaf1a917c63309e73a3c22",
+    "url": "/CRA/index.html"
+  },
+  {
+    "revision": "973f2defdd98bd6e9377",
+    "url": "/CRA/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "9f4a02e2f2eb96d28cc8",
+    "url": "/CRA/static/css/main.5e9f3235.chunk.css"
+  },
+  {
+    "revision": "973f2defdd98bd6e9377",
+    "url": "/CRA/static/js/2.12ab3b22.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/CRA/static/js/2.12ab3b22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f4a02e2f2eb96d28cc8",
+    "url": "/CRA/static/js/main.c6c7888c.chunk.js"
+  },
+  {
+    "revision": "52db32d89f1df7b2262c",
+    "url": "/CRA/static/js/runtime-main.96ea509a.js"
+  }
+]);
